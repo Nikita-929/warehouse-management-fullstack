589bef5870e601cca2330502d80a110c1ff217c5
Nimbus <mahatonikita929@gmail.com>
2025-11-21 15:25:00 +0530

    Update SSLConfig.java

--- backend/src/main/java/com/warehouse/config/SSLConfig.java
+++ backend/src/main/java/com/warehouse/config/SSLConfig.java
@@ -1,4 +1,4 @@
-package com.yourapp.config;
+package com.warehouse.config;
 
 import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Component;
@@ -10,11 +10,14 @@ public class SSLConfig {
 
     @PostConstruct
     public void init() throws Exception {
-        String caCert = System.getenv("AIVEN_CA_CERT");
+        String caCert = System.getenv("DB_SSL_CA");
         if (caCert != null) {
-            FileWriter fw = new FileWriter("aiven-ca.pem");
-            fw.write(caCert);
-            fw.close();
+            FileWriter writer = new FileWriter("aiven-ca.pem");
+            writer.write(caCert);
+            writer.close();
+            System.out.println("Aiven CA certificate written to aiven-ca.pem");
+        } else {
+            System.out.println("DB_SSL_CA is not set. SSL will fail.");
         }
     }
 }
